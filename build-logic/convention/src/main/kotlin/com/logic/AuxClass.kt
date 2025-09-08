14684b5c0971b84a1025ad056c389f12affb8318
Inaki Villar <inaki.seri@gmail.com>
2025-09-08 14:40:48 -0700

    test @

---
+++ build-logic/convention/src/main/kotlin/com/logic/AuxClass.kt
@@ -0,0 +1,18 @@
+package com.logic
+
+class AuxClass {
+
+    fun alo() {
+
+    }
+@Suppress("unused")
+    private fun nonAbiBump_1(): Int {
+        // auto-generated non-ABI function for iteration 1 at 2025-09-08T21:39:37Z
+        return 1
+    }
+@Suppress("unused")
+    private fun nonAbiBump_2(): Int {
+        // auto-generated non-ABI function for iteration 2 at 2025-09-08T21:40:13Z
+        return 2
+    }
+}
