62347bf2b4b939ffa768450faed1102d697ef23d
Inaki Villar <inaki.seri@gmail.com>
2025-09-09 12:02:08 -0700

    a

--- build-logic/convention/src/main/kotlin/com/logic/AwesomeTask.kt
+++ build-logic/convention/src/main/kotlin/com/logic/AwesomeTask.kt
@@ -8,9 +8,6 @@ abstract class AwesomeTask : DefaultTask() {
     @TaskAction
     fun doSomeWork(){
         println(1)
-        println(1)  // iter 1 at 2025-09-09T18:48:18Z
-        println(2)  // iter 2 at 2025-09-09T18:51:51Z
-        println(3)  // iter 3 at 2025-09-09T18:54:58Z
         // add content here
     }
 
