ffc3f4882ac5fa809d04a63a4cad19770872b565
Inaki Villar <inaki.seri@gmail.com>
2025-09-09 13:09:50 -0700

    help

--- change.sh
+++ change.sh
@@ -44,7 +44,7 @@ update_do_some_work() {
 run_build() {
   local tag="$1"
   echo ">>> $(date -u +%FT%TZ) Running assembleDebug"
-  ./gradlew assembleDebug -Dscan.tag.$tag --info
+  ./gradlew help -Dscan.tag.$tag --info
 }
 
 # === Main ===
