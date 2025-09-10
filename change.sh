cf7acbd0b4f51f5ef9816f693da2e51a254c00db
Inaki Villar <inaki.seri@gmail.com>
2025-09-09 17:45:02 -0700

    A

--- change.sh
+++ change.sh
@@ -55,7 +55,7 @@ run_build seed2
 for ((i=1; i<=ITERATIONS; i++)); do
   echo "===== CYCLE $i ====="
   update_do_some_work "$i"
-  run_build incremental_change_9
+  run_build incremental_change_8_14
 done
 
 echo "===== FINAL BUILD ====="
